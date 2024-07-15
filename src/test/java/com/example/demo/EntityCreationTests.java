aa65695a634e231a659e9fcc2b6b7408a0eb3be7
John Steel <john@jskw.dev>
2024-07-15 09:42:30 -0400

    add tests

--- src/test/java/com/example/demo/EntityCreationTests.java
+++ src/test/java/com/example/demo/EntityCreationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.rest.entities.Dog;
 import com.example.demo.rest.entities.Human;
 import com.example.demo.rest.entities.Passport;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +13,9 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Objects;
+import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -42,12 +44,12 @@ class EntityCreationTests {
         // List Passports and verify creation
         var response = restTemplate.getForEntity(baseUrl("/passports"), CollectionModel.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertTrue(response.getBody().getContent().stream().anyMatch(p -> p.toString().startsWith("{id=%d,".formatted(createdPassport.getId()))));
+        assertThat(listContainsId(response, createdPassport.getId())).isTrue();
 
         // Get Passport by ID and verify
         ResponseEntity<Passport> getByIdResponse = restTemplate.getForEntity(baseUrl("/passports/" + createdPassport.getId()), Passport.class);
         assertEquals(HttpStatus.OK, getByIdResponse.getStatusCode());
-        assertEquals(createdPassport.getId(), Objects.requireNonNull(getByIdResponse.getBody()).getId());
+        assertEquals(createdPassport.getId(), getId(getByIdResponse));
     }
 
     @Test
@@ -64,9 +66,57 @@ class EntityCreationTests {
 
     @Test
     void createHuman() {
-        Human human = Human.builder().name("John Doe").dna(new byte[]{1,2,3}).build();
-        ResponseEntity<Human> response = restTemplate.postForEntity(baseUrl("/animals"), human, Human.class);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        // Additional assertions to verify the Human entity
+        var passport = Passport.builder().country("Canada").expirationYear(2030).issueYear(2020).build();
+        var createPassportResponse = restTemplate.postForEntity(baseUrl("/passports"), passport, Passport.class);
+        var human = Human.builder().name("Jane Doe").dna(new byte[]{1,2,3}).passports(List.of(passport)).build();
+        var createHumanResponse = restTemplate.postForEntity(baseUrl("/animals"), human, Human.class);
+
+        var listPassportResponse = restTemplate.getForEntity(baseUrl("/passports"), CollectionModel.class);
+        var listHumanResponse = restTemplate.getForEntity(baseUrl("/animals"), CollectionModel.class);
+        var getByIdResponse = restTemplate.getForEntity(baseUrl("/animals/" + getId(createHumanResponse)), Human.class);
+        var getPassportByIdResponse = restTemplate.getForEntity(baseUrl("/passports/" + getId(createPassportResponse)), Passport.class);
+        var getPassportByHumanResponse = restTemplate.getForEntity(baseUrl("/animals/" + getId(createHumanResponse) + "/passports"), CollectionModel.class);
+        var getHumanByPassportResponse = restTemplate.getForEntity(baseUrl("/passports/" + getId(createPassportResponse) + "/owner"), Human.class);
+
+        SoftAssertions.assertSoftly(assertions -> {
+            assertions.assertThat(createPassportResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+            assertions.assertThat(createHumanResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+            assertions.assertThat(listPassportResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertions.assertThat(listContainsId(listPassportResponse, getId(createPassportResponse))).isTrue();
+
+            assertions.assertThat(listHumanResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertions.assertThat(listContainsId(listHumanResponse, getId(createHumanResponse))).isTrue();
+
+            assertions.assertThat(getByIdResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertions.assertThat(getId(getByIdResponse)).isEqualTo(getId(createHumanResponse));
+
+            assertions.assertThat(getPassportByIdResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertions.assertThat(getId(getPassportByIdResponse)).isEqualTo(getId(createPassportResponse));
+
+            assertions.assertThat(getPassportByHumanResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertions.assertThat(listContainsId(getPassportByHumanResponse, getId(createPassportResponse))).isTrue();
+
+            assertions.assertThat(getHumanByPassportResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertions.assertThat(getId(getHumanByPassportResponse)).isEqualTo(getId(createHumanResponse));
+        });
+    }
+
+    private static long getId(ResponseEntity<?> getHumanByPassportResponse) {
+        if (getHumanByPassportResponse.getBody() instanceof Human)
+            return ((Human) getHumanByPassportResponse.getBody()).getId();
+
+        if (getHumanByPassportResponse.getBody() instanceof Passport)
+            return ((Passport) getHumanByPassportResponse.getBody()).getId();
+
+        return -1;
+    }
+
+    public static boolean listContainsId(ResponseEntity<CollectionModel> response, Long id) {
+        if (response.getBody() == null) {
+            return false;
+        }
+        return response.getBody().getContent().stream()
+                .anyMatch(entity -> entity.toString().contains("id=" + id + ","));
     }
 }
